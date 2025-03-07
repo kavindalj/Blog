88d5909e8203b1e13629e2702af17610084911fa
Kavinda L Jayarathna <kavindaljayarathna725@gmail.com>
2025-03-07 14:49:29 +0530

    build:make getPosts data function for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+import Posts from "./components/Posts";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+async function getPosts() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const markdownWithMetadata = fs.readFileSync(path.join("posts", filename), "utf-8");
+    const { data: frontmatter } = matter(markdownWithMetadata);
+    return {
+      slug,
+      title: frontmatter.title || "Untitled",
+      author: frontmatter.author || "Unknown",
+      date: frontmatter.date ? new Date(frontmatter.date).toISOString().split("T")[0] : "No Date",
+    };
+  });
+  return { posts };
+}
+
+export default async function Home() {
+  const { posts } = await getPosts();
+
+  console.log(posts);
+
   return (
     <>
-      <h1>Home</h1>
+      <Posts posts={posts} />
     </>
   );
 }
+
