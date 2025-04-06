da9c13fb89bfa94177e2a2913719baf8457506ef
Kavinda L Jayarathna <128674785+kavindalj@users.noreply.github.com>
2025-04-06 14:08:43 +0530

    fix: errors in slug/page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,30 +1,36 @@
-import { notFound } from "next/navigation";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { marked } from "marked";
+import { notFound } from "next/navigation";
 import Image from "next/image";
-import React from "react";
 
-export default async function PostPage({ params }: { params: Record<string, string> }) {
-  if (!params?.slug) {
-    notFound();
-  }
+interface Props {
+  params: Promise<{ slug: string }>;
+}
 
-  const slug = params.slug;
+export async function generateStaticParams() {
+  const files = fs.readdirSync(path.join("posts"));
+  return files.map((filename) => ({
+    slug: filename.replace(".md", ""),
+  }));
+}
+
+export const runtime = "nodejs";
+export const dynamic = "force-static";
+
+
+export default async function PostPage({ params }: Props) {
+  const { slug } = await params;
   const filePath = path.join("posts", `${slug}.md`);
 
   if (!fs.existsSync(filePath)) {
     notFound();
   }
 
-  const markdownWithMetadata = fs.readFileSync(filePath, "utf-8");
-  const { data: frontmatter, content } = matter(markdownWithMetadata);
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const { data: frontmatter, content } = matter(fileContent);
   const htmlContent = marked(content);
-
-  const formattedDate = frontmatter.date
-    ? new Date(frontmatter.date).toISOString().split("T")[0]
-    : "No Date";
 
   return (
     <div className="container mx-auto mt-3 p-4 min-h-[calc(100vh-8rem)]">
@@ -35,11 +41,18 @@ export default async function PostPage({ params }: { params: Record<string, stri
           width={1000}
           height={250}
         />
-        <h1 className="text-3xl text-gray-800 font-bold">{frontmatter.title}</h1>
-        <p className="text-gray-600">{formattedDate}</p>
+        <h1 className="text-4xl font-bold text-gray-900">
+          {frontmatter.title}
+        </h1>
+        <p className="text-gray-600">
+          {frontmatter.date
+            ? new Date(frontmatter.date).toISOString().split("T")[0]
+            : "No Date"}
+        </p>
       </div>
+
       <div
-        className="prose prose-xl max-w-none markdown-content mt-4"
+        className="prose prose-xl max-w-none mt-4 markdown-content"
         dangerouslySetInnerHTML={{ __html: htmlContent }}
       />
     </div>
